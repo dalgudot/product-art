55750c9b3ac8a2bc1553ed8ddf6ab52e76846df3
dalgudot <dalgudot@gmail.com>
2023-05-21 13:26:24 +0900

    update cds package config

--- apps/dalgu/src/app/page.tsx
+++ apps/dalgu/src/app/page.tsx
@@ -1,11 +1,12 @@
+// 'use client';
+
 import { Button, Header } from '@dalgu/core-design-system';
 import styles from './page.module.css';
-// import { Button, Header } from '@dalgu/core-design-system';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header text='hiHi' />
+      <Header text='dalgu' />
       <Button />
     </main>
   );
--- packages/core-design-system/README.md
+++ packages/core-design-system/README.md
@@ -1,11 +1,1 @@
-Run the following command for publishing to NPM:
-
-```sh
-pnpm build
-```
-
-```sh
-pnpm publish
-```
-
-[Bundling] (https://turbo.build/repo/docs/handbook/publishing-packages/bundling)
+Coming Soon...
