ed5a10d56670ca04b5bd109d11c050d41a576c6b
dalgudot <dalgudot@gmail.com>
2023-06-06 13:34:16 +0900

    add yoonseul app & set i18n / theme for all project

---
+++ apps/yoonseul/app/[locale]/layout.tsx
@@ -0,0 +1,27 @@
+import '@dalgu/core-design-system/shared/colors-1.css';
+import '@dalgu/core-design-system/shared/text-styles-1.css';
+import '@dalgu/core-design-system/src/foundations/globals.css';
+import { useLocale } from 'next-intl';
+import { Providers } from './providers';
+
+export const metadata = {
+  title: 'KyungHwan Kim',
+  description: 'Product Designer',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const locale = useLocale();
+
+  return (
+    // suppressHydrationWarning은 next-theme 에러 방지 위해 추가
+    <html lang={locale} suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
