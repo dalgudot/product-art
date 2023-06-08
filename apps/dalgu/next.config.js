8aebfa79be18b62934157fecc2f894520f2de9ea
dalgudot <dalgudot@gmail.com>
2023-06-08 19:32:51 +0900

    Add redirect in dalgu.app

--- apps/dalgu/next.config.js
+++ apps/dalgu/next.config.js
@@ -10,6 +10,34 @@ const nextConfig = {
   // 번들되지 않은 Internal Packages를 Next.js 앱으로 가져올 때 발생하는 오류 방지하기 위한 Config
   // https://turbo.build/repo/docs/handbook/sharing-code/internal-packages#6-configuring-your-app
   transpilePackages: ['@dalgu/core-design-system'],
+
+  async redirects() {
+    return [
+      {
+        source: '/yoonseul',
+        destination: 'https://apps.apple.com/kr/app/yoonseul/id1618657913',
+        permanent: false,
+      },
+      {
+        source: '/today-todo',
+        destination:
+          'https://apps.apple.com/kr/app/today-to-do-success-secret/id1622514232',
+        permanent: false,
+      },
+      {
+        source: '/plant-family',
+        destination:
+          'https://apps.apple.com/kr/app/plant-family-water-reminder/id1633937579',
+        permanent: false,
+      },
+      {
+        source: '/reminder',
+        destination:
+          'https://apps.apple.com/kr/app/reminder-to-do-list/id6444939279',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
