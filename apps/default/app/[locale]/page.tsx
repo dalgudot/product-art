3a5001ee051f43e74b8a03632831c1426bc5f796
dalgudot <dalgudot@gmail.com>
2023-06-08 19:43:00 +0900

    add default app

---
+++ apps/default/app/[locale]/page.tsx
@@ -0,0 +1,22 @@
+import { useTranslations } from 'next-intl';
+import Link from 'next-intl/link';
+import ThemeToggle from '../../components/ThemeToggle';
+import { Button } from '@dalgu/core-design-system';
+
+export default function Home() {
+  const t = useTranslations('Index');
+
+  return (
+    <>
+      <h1>{t('title')}</h1>
+      Default
+      <ThemeToggle />
+      <Link href='/' locale='ko'>
+        Switch to Korean
+      </Link>
+      <Link href='/' locale='en'>
+        Switch to English
+      </Link>
+    </>
+  );
+}
