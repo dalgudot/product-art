10a85b14fabfc23f0babaf24e51e3ee28e158d74
dalgudot <dalgudot@gmail.com>
2023-09-28 12:33:49 +0900

    update reminder home

--- apps/dalgu/app/[locale]/reminder/page.tsx
+++ apps/dalgu/app/[locale]/reminder/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+
 import type { NextPage } from 'next';
+import IconReminderLogo24 from '../../../components/svg/icon-reminder-logo-24';
+import s from './page.module.css';
+import { Button } from '@dalgu/core-design-system';
+import { useRouter } from 'next/navigation';
 
 const ReminderHome: NextPage = () => {
+  const router = useRouter();
+
+  function goToReminderAppStore() {
+    router.push(
+      'https://apps.apple.com/kr/app/reminder-to-do-list/id6444939279'
+    );
+  }
+
   return (
-    <main>
-      <h1>리마인더. 필요한 기능만.</h1>
+    <main className={s.main}>
+      <IconReminderLogo24 />
+      <h1>Reminder</h1>
+      <p>Only What You Need</p>
+      <Button
+        label='Learn More'
+        onClick={goToReminderAppStore}
+        buttonColor='var(--gray6)'
+      />
     </main>
   );
 };
